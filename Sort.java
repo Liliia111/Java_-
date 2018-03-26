b66e13daac222bd353d5377ae0f14b14b8438abb
Liliia111 <34775878+Liliia111@users.noreply.github.com>
2018-03-26 15:14:32 +0300

    Add files via upload

---
+++ Sort.java
@@ -0,0 +1,41 @@
+package Sort;
+
+public class Sort {
+	
+		private static void sort(int[] array, Comparator comp) {
+			for (int gap = array.length / 2; gap > 0; gap /= 2) {
+				for (int i = gap; i < array.length; i++) {
+					int val = array[i];
+					int j;
+					for (j = i; j >= gap && comp.compare(array[j - gap], val) > 0; j -= gap) {
+						array[j] = array[j - gap];
+					}
+					array[j] = val;
+				}
+			}
+		}
+		
+		      
+
+		public static void main(String[] args){
+			int[] array = {1,5,2,4,10,6,0,3,10};
+			Comparator comp = new Comparator();
+			
+			//Change your code here
+			sort(array, comp);
+			for(int i= 0; i <  array.length/2; ++i) {
+			    int tmp = array[i];
+			    array[i] = array[array.length - 1 - i];
+			    array[array.length - 1 - i] = tmp;
+			}
+			
+			
+			for (int i = 0; i < array.length; i++) {
+				System.out.print(array[i] + " ");
+			}
+		}
+		
+	}      
+	
+
+
